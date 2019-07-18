392228ce3126ecb2371f991fd691541929e0a50a
inyhsu <isingridsu@gmail.com>
2019-07-18 13:41:16 -0700

    Linked Lists Completed

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,84 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    /*
+    Add 
+      - add node to list
+      - set node.value to 'value'
+      - set next property of list.tail to current node 
+      - update list.tail to current node
+      - If list is empty, set list.node to list.head 
+    */
+    
+    let node = new Node(value);
+    node.value = value;
+    
+    if(!list.tail){
+      
+      list.tail = node;
+      list.head = node;
+    
+    }else{
+      
+      list.tail.next = node;
+      list.tail = node;
+    }
+    
   };
 
   list.removeHead = function() {
+    
+    /*
+    Remove
+      - If there is no head return {}
+      - If there is head, make next node as head
+        - Save our list.head to another var pop
+        - Update list.head = list.head.next
+        - delete list.head
+      - return var pop
+    */
+    
+    if(!list.head){
+      
+      return {}
+    
+    }else{
+      
+      let pop = list.head.value;
+      list.head = list.head.next;
+      return pop;
+    }
   };
 
   list.contains = function(target) {
+    
+    /*
+    Contains
+      - If head.value = target , return true
+      - If head.value !== target and head.next exist, call list.contains on head.next
+      - If head.value !== target and head.next === null, return false
+    */
+    
+    var move = function(node) {
+      if(node.value === target){
+        return true;
+        
+      }else{
+        
+        if(node.next !== null){
+          return move(node.next)
+        }else{
+          return false;
+        }
+      }
+    }
+    
+    if(!list.head){
+      return false;
+    }else{
+      return move(list.head);
+    }
+    
   };
 
   return list;
@@ -23,7 +95,3 @@ var Node = function(value) {
 
   return node;
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
