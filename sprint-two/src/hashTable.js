aea7f2320dc6e220d008a263ac3d2b9819f980ee
inyhsu <isingridsu@gmail.com>
2019-07-20 09:31:56 -0700

    Implement set with HT method

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -50,6 +50,10 @@ HashTable.prototype.remove = function(k) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  - Insert: O(1)
+  - Retrieve: O(1)
+  - Remove: O(1)
+ 
  */
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,34 +1,45 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = []; // fix me
+  set._storage = new HashTable(); // fix me
+  set.key = 1;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.insert(item, this.key);
+  this.key += 1;
   
-  if(!this._storage.includes(item)) {
-    this._storage.push(item);
-  }
+  // if(!this._storage.includes(item)) {
+  //   this._storage.push(item);
+  // }
 };
 
 setPrototype.contains = function(item) {
-  if (this._storage.indexOf(item) >= 0) {
+  
+  if (this._storage.retrieve(item)) {
     return true;
   }
+  // if (this._storage.indexOf(item) >= 0) {
+  //   return true;
+  // }
   return false;
 };
 
 setPrototype.remove = function(item) {
-  for(var i = 0; i < this._storage.length; i++) {
-    if(this._storage[i] === item) {
-      this._storage.splice(i,1);
-    }
-  }
+  // for(var i = 0; i < this._storage.length; i++) {
+  //   if(this._storage[i] === item) {
+  //     this._storage.splice(i,1);
+  //   }
+  // }
   
+  this._storage.remove(item);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ - Add: O(1)
+ - Contains: O(1)
+ - Remove: O(1)
  */
